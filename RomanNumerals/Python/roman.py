b3d92db1f46540cbde239375d6b701641d9f277e
DallasEpperson <depperson@geninfo.com>
2013-09-18 11:46:05 -0400

    Python version of roman numerals can convert both directions

--- RomanNumerals/Python/roman.py
+++ RomanNumerals/Python/roman.py
@@ -15,11 +15,55 @@ def RomanToInt(roman):
         stringCursor += 1
     return returnInt
 
+def IntToRoman(integer):
+    returnString = ""
+    valueRemaining = integer
+    validRomanCombinations = RomanNumeralValues.copy()
+
+    #build up a list of valid roman combinations
+    for key in RomanNumeralValues:
+        for prefixKey in RomanNumeralValues:
+            if prefixKey != "N":
+                if IsPowerOfTen(RomanNumeralValues[prefixKey]):
+                    if RomanNumeralValues[prefixKey] < RomanNumeralValues[key]:
+                        if RomanNumeralValues[prefixKey] >= RomanNumeralValues[key]/10:
+                            validRomanCombinations[prefixKey + key] = RomanNumeralValues[key] - RomanNumeralValues[prefixKey]
+    
+    #sort our list of valids by value, descending
+    combos = sorted(validRomanCombinations.iteritems(), key=lambda item: -item[1])
+    
+    while valueRemaining > 0:
+        #Find highest value (first in reverse sorted list) we can subtract
+        for k,v in combos:
+            if v <= valueRemaining:
+                returnString += k
+                valueRemaining -= v
+                break
+        
+    return returnString
+
+def IsPowerOfTen(integer):
+    a = integer
+    while a > 1:
+        a = a/10
+    return a==1
+
+def IsInteger(inString):
+    try:
+        int(inString)
+        return True
+    except ValueError:
+        return False
+
 request = ""
 while True:
-    print "Input a Roman Numeral (or exit to quit)"
+    print "Input an integer, a Roman Numeral, or 'exit' to quit)"
     request = raw_input("> ")
     if request != "exit":
-        print request + " = " + str(RomanToInt(request))
+        if IsInteger(request):
+            print request + " = " + IntToRoman(int(request))
+        else:
+            print request + " = " + str(RomanToInt(request))
     else:
         exit()
+
--- RomanNumerals/README.md
+++ RomanNumerals/README.md
@@ -1,2 +1,5 @@
 CodeKatas - Roman Numerals
 =========
+Convert Roman Numerals to standard integers and vice versa.  
+Do not store pre-computed values (such as IV = 4), however a table of numeral definitions is allowed (such as X = 10).  
+Input error checking is not required.
